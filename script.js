ba468d6e089d648ae9b61172026633c9ffa48317
LouisMRC <36147361+LouisMRC@users.noreply.github.com>
2020-10-20 03:11:09 +0200

    Preparations

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
 const Discord = require("discord.js");
+const Mariadb = require("mariadb");
 const Client = new Discord.Client();
+const LANGUAGES = new Map();
 const{
-    token
+    token,
+    langPath,
+    dbHost,
+    dbName,
+    dbUsername,
+    dbUserPasswd
 } = require('./config.json');
 
-Client.once("ready", () => console.log("Let's Go!!!"));
+const pool = Mariadb.createPool({
+    host: dbHost,
+    user: dbUsername,
+    password: dbUserPasswd,
+    database: dbName,
+    connectionLimit: 5
+});
 
-Client.on("message", (message) => {
-    const args = message.content.slice(prefix.length).split(/ +/);
-    switch(args[0])
-    {
+let servers = [];
+
+pool.getConnection()
+    .then(connection => {
+        Client.once("ready", () => console.log("Let's Go!!!"));
+
+        Client.on("message", (message) => {
+            const args = message.content.slice(prefix.length).split(' ');
+            switch(args[0].toLowerCase())
+            {
+                case "count":
+                    break;
+                case "role":
+                    switch(args[1].toLowerCase())
+                    {
+                        case "add":
+                            break;
+                        case "role":
+                            break;
+                        case "replace":
+                            break;
+                    }
+                    break;
+            }
+        })
         
-    }
-})
+        Client.login(token);
+    }).catch(err => console.error(err));
 
-Client.login(token);
+function loadLanguages()
+{
+    LANGUAGES.set("en", require(langPath + "en.json"));
+    LANGUAGES.set("fr", require(langPath + "fr.json"));
+}
